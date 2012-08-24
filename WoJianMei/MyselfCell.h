b42ddcdd3408263da99af953f6653fbfee12a97e
Tom Callon <tomcallon2010@gmail.com>
2012-08-24 15:02:03 +0800

    commit

---
+++ WoJianMei/MyselfCell.h
@@ -0,0 +1,13 @@
+//
+//  MyselfCell.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 8/23/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyselfCell : UITableViewCell
+
+@end
---
+++ WoJianMei/TomCallonTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TomCallonTableViewCell.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 8/23/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TomCallonTableViewCell : UITableViewCell
+
+@end
